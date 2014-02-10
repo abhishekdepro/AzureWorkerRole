cb3508282789ae1402f51c6c6bc184af600da8dc
Abhishek De <abhishekde@hotmail.com>
2014-02-10 18:15:31 +0530

    Free

--- script.sh
+++ script.sh
@@ -3,14 +3,17 @@
 # This script opens 4 terminal windows.
 
 now=0
-
+count=0
 echo $now
 i=12
 p=$now
 while true
 do
 now=$(date +"%M")
-if [ $(( $now % 10  )) -eq "0" ]; then
-    python /home/abhishek/AzureWorkerRole/AzureTableStore.py
+if [ $(( $now % 100  )) -eq "0" ]; then
+count=1
+	if [ $count -eq "0" ]; then
+		python /home/abhishek/AzureWorkerRole/AzureTableStore.py
+	fi
 fi
 done
