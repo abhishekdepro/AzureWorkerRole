12d2e54cc5b85f1646c26c7721d4643e8ade8b54
Abhishek De <abhishekde@hotmail.com>
2014-02-10 16:28:47 +0530

    Changes

--- AzureTableStore.py
+++ AzureTableStore.py
@@ -12,78 +12,77 @@ table_service = TableService(account_name='asthmacure', account_key='zryh9UWR1w2
 
 
 
-while(1):
-    if(int((datetime.datetime.now().strftime("%M")))%10==0):
-        class Temperature(object):
-            def toCelcius(self, deg_f):
-                return (deg_f-32)*5/9
 
-        #minute=minute+1
-        city='kolkata'
-        url = "http://api.openweathermap.org/data/2.5/weather?q=kolkata&mode=xml&units=metric"
-        source=urllib2.urlopen(url)
-        tomorrow=str(datetime.date.today()+datetime.timedelta(days=1))
-        htmltext=source.read()
-        print("<------------------------WEATHER REPORT: "+city.upper()+" for "+tomorrow+" ------------>")
+class Temperature(object):
+    def toCelcius(self, deg_f):
+        return (deg_f-32)*5/9
 
-        # search for pattern using regular expressions (.+?)
-        #if(htmltext.find(adate)!=-1):
-        direction='<direction value="(.+?)" code="(.+?)" name="(.+?)"/>'
-        wind='<speed value="(.+?)" name="(.+?)"/>'
-        temp='<temperature value="(.+?)" min="(.+?)" max="(.+?)" unit="(.+?)"/>'
-        humidity='<humidity value="(.+?)" unit="%"/>'
-        condition='<weather number="(.+?)" value="(.+?)" icon="(.+?)"/>'
-        pattern_direction=re.compile(direction)
-        pattern_wind=re.compile(wind)
-        pattern_temp=re.compile(temp)
-        pattern_humidity=re.compile(humidity)
-        pattern_cond=re.compile(condition)
+#minute=minute+1
+city='kolkata'
+url = "http://api.openweathermap.org/data/2.5/weather?q=kolkata&mode=xml&units=metric"
+source=urllib2.urlopen(url)
+tomorrow=str(datetime.date.today()+datetime.timedelta(days=1))
+htmltext=source.read()
+print("<------------------------WEATHER REPORT: "+city.upper()+" for "+tomorrow+" ------------>")
 
-        
-        
-        # match pattern with htmltext
-        weather_windspeed=re.findall(pattern_wind,htmltext)
-        weather_winddirection=re.findall(pattern_direction,htmltext)
-        weather_temp=re.findall(pattern_temp,htmltext)
-        weather_cond=re.findall(pattern_cond,htmltext)
-        weather_humid=re.findall(pattern_humidity,htmltext)
+# search for pattern using regular expressions (.+?)
+#if(htmltext.find(adate)!=-1):
+direction='<direction value="(.+?)" code="(.+?)" name="(.+?)"/>'
+wind='<speed value="(.+?)" name="(.+?)"/>'
+temp='<temperature value="(.+?)" min="(.+?)" max="(.+?)" unit="(.+?)"/>'
+humidity='<humidity value="(.+?)" unit="%"/>'
+condition='<weather number="(.+?)" value="(.+?)" icon="(.+?)"/>'
+pattern_direction=re.compile(direction)
+pattern_wind=re.compile(wind)
+pattern_temp=re.compile(temp)
+pattern_humidity=re.compile(humidity)
+pattern_cond=re.compile(condition)
 
-        
 
-        #print "Overall Weather status: ",weather_cond[0][1]
-        RowKey=datetime.datetime.now().strftime("%d%m%Y%H%M")
-        temperature = weather_temp[0][0]
-        #print "Minimum Temperature: ",weather_temp[0][1]
-        #print "Maximum Temperature: ",weather_temp[0][2]
-        print "Wind Direction: ",weather_windspeed[0][0]           
-            
-        
-        def checkduplicate():
-            tasks = table_service.query_entities('WeatherFetch', "PartitionKey eq 'data'")
-            flag=0
-            
-            for task in tasks:
-                if(task.RowKey==RowKey):
-                    flag=1
-                elif(task.temperature==temperature):
-                    flag=1
 
-            if(flag==1):
-                return True
-            else:
-                return False
-                
-            
+# match pattern with htmltext
+weather_windspeed=re.findall(pattern_wind,htmltext)
+weather_winddirection=re.findall(pattern_direction,htmltext)
+weather_temp=re.findall(pattern_temp,htmltext)
+weather_cond=re.findall(pattern_cond,htmltext)
+weather_humid=re.findall(pattern_humidity,htmltext)
+
+
+
+#print "Overall Weather status: ",weather_cond[0][1]
+RowKey=datetime.datetime.now().strftime("%d%m%Y%H%M")
+temperature = weather_temp[0][0]
+#print "Minimum Temperature: ",weather_temp[0][1]
+#print "Maximum Temperature: ",weather_temp[0][2]
+print "Wind Direction: ",weather_windspeed[0][0]           
     
 
-        
-        
-        
-        
-        if(not checkduplicate()):
-            fetched_data = {'PartitionKey': 'data', 'RowKey': RowKey, 'temperature' : temperature,'min_temperature' : weather_temp[0][1], 'humidity' : weather_humid[0],'windspeed':weather_windspeed[0][0],'winddirection':weather_winddirection[0][1] ,'condition':weather_cond[0][1]}
-            table_service.insert_entity('WeatherFetch', fetched_data)
-        else:
-            print 'Fuck off'
-
+def checkduplicate():
+    tasks = table_service.query_entities('WeatherFetch', "PartitionKey eq 'data'")
+    flag=0
     
+    for task in tasks:
+        if(task.RowKey==RowKey):
+            flag=1
+        elif(task.temperature==temperature):
+            flag=1
+
+    if(flag==1):
+        return True
+    else:
+        return False
+        
+    
+
+
+
+
+
+
+if(not checkduplicate()):
+    fetched_data = {'PartitionKey': 'data', 'RowKey': RowKey, 'temperature' : temperature,'min_temperature' : weather_temp[0][1], 'humidity' : weather_humid[0],'windspeed':weather_windspeed[0][0],'winddirection':weather_winddirection[0][1] ,'condition':weather_cond[0][1]}
+    table_service.insert_entity('WeatherFetch', fetched_data)
+else:
+    print 'Fuck off'
+
+
